16c2a2e81f712f3b0e154975187e23907a8db146
unknown <ayush.udml@gmail.com>
2024-03-20 22:41:16 +0530

    fixed front-end

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ cloudinary.config({
 
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
-app.use(express.static(path.resolve(__dirname, "./public")));
+app.use(express.static(path.resolve(__dirname, "./client/dist")));
 
 app.use(morgan("dev"));
 app.use(express.json());
@@ -41,7 +41,7 @@ app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", authenticateUser, userRouter);
 
 app.get("*", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "./public", "index.html"));
+  res.sendFile(path.resolve(__dirname, "./client/dist", "index.html"));
 });
 
 app.use("*", (req, res) => {
